c4b5beb6eacf489241f0c809e2659b0aeb28f622
bishwamittra <bishwa12buet@gmail.com>
2023-11-29 09:21:20 +0800

    non-split exp

--- fusionnet_split_learning/todo_non_split.sh
+++ fusionnet_split_learning/todo_non_split.sh
@@ -4,7 +4,7 @@ epoch="100"
 for i in {1..3}
 do
     seed=$((client_seed + i))
-    python main.py --epoch ${epoch} --seed ${seed} --device cuda --cuda_id 1 --save_root result
+    python main.py --epoch ${epoch} --seed ${seed} --device cpu --cuda_id 1 --save_root result_non_split
 done
 
 
